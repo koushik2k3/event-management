47853b0f7d302a7dae43f4e64871dadd5563811a
Koushik KP <koushikkp2003@gmail.com>
2024-06-24 16:05:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById('eventCreationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-
+    event.preventDefault(); 
     let eventName = document.getElementById('eventName').value;
     let eventDate = document.getElementById('eventDate').value;
 
@@ -8,5 +7,5 @@ document.getElementById('eventCreationForm').addEventListener('submit', function
 
 
     events.push({ name: eventName, date: eventDate });
-    renderEvents(events); // Update events list
+    renderEvents(events); 
 });
