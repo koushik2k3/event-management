a7e180118755cd14ee9f51ce4312ccf8aa30047f
Koushik KP <koushikkp2003@gmail.com>
2024-06-24 15:56:59 +0530

    Deploying to GitHub Pages

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 document.getElementById('eventCreationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Retrieve form data
+    event.preventDefault();
     let eventName = document.getElementById('eventName').value;
     let eventDate = document.getElementById('eventDate').value;
 
-    // Example: In a real application, send data to backend for storage
     console.log(`Event Name: ${eventName}, Event Date: ${eventDate}`);
 
-    // Clear form fields (optional)
     this.reset();
 
-    // Simulate adding new event to the list (for demo purposes)
     events.push({ name: eventName, date: eventDate });
-    renderEvents(events); // Update events list
+    renderEvents(events); 
 });
